78df1b3a0f60bb5442f6b998c479e9ffbec3df1d
h <145804736+nhithub4@users.noreply.github.com>
2024-08-20 06:00:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))  # استخدم المنفذ 5000 كافتراضي إذا لم يتم تعيين PORT
-    app.run(host='0.0.0.0', port=port)
