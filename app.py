ef23083af7cb775345a5c7cb3397973bcd4301d1
h <145804736+nhithub4@users.noreply.github.com>
2024-08-20 05:58:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,7 +8,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    # تشغيل الخادم على جميع العناوين (0.0.0.0) وعلى المنفذ المحدد من متغير البيئة PORT
-    import os
     port = int(os.getenv("PORT", 5000))  # استخدم المنفذ 5000 كافتراضي إذا لم يتم تعيين PORT
     app.run(host='0.0.0.0', port=port)
