7c4da125248d63adea2340e43b803e05f6dfef09
h <145804736+nhithub4@users.noreply.github.com>
2024-08-20 11:14:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/health')
+def health():
+    return jsonify(status="healthy"), 200
+
+if __name__ == "__main__":
+    app.run()
