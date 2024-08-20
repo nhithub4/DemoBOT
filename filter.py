d858dc8dd020350dc60f8fee272c11c19c3343e1
h <145804736+nhithub4@users.noreply.github.com>
2024-08-20 10:01:03 +0300

    Update filter.py

--- filter.py
+++ filter.py
@@ -5,7 +5,7 @@ from telegram.ext import CallbackContext, MessageHandler, filters
 
 # إعدادات تسجيل الأحداث
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
